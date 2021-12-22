0309f43188a6fc7275139a087f9c0349bd5304a8
Conner Rhodes <connerrhodes.25@gmail.com>
2021-12-22 16:51:22 -0500

    initial stylying

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { getCategories } from '../services'
+import {logo} from '../public/Logo.png'
 
     
 const Header = () => {
@@ -15,24 +16,15 @@ const Header = () => {
             <div className='border-b w-full inline-block border-blue-400 py-8'>
                 <div className='md:float-left block'>
                         <Link href='/'>
-                            <span className='cursor-pointer font-bold text-4xl text-white'>
-                                Inexplicitus
+                            <span className='cursor-pointer font-bold text-4xl text-amber-500'>
+                                Inexplicitus Times: The News of Today, Inexplained!
                             </span>
                         </Link>
                 </div>
-                <div className = "hidden md:float-left md:contents">
-                    {categories.map((category)=> (
-                    <Link key={category.slug} href={`/category/${category.slug}`}>
-                        <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
-                            {category.name}
-                        </span>
-                    </Link>
-                    ))}
-
+                <div className='md:float-left block'>
                 </div>
             </div>
-            
-        </div>
+            </div>
     )
 }
 
